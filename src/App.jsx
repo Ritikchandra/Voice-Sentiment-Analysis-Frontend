590443e8cfe590d7717199cfa2a863cf6ede6200
Ritikchandra <ritik.chandra009@gmail.com>
2024-07-16 16:14:50 +0530

    Add Extract CSV

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import "./App.css"
+import "./App.css";
+
 const App = () => {
-  const [file, setFile] = useState(null);
-  const [fileName, setFileName] = useState('');
-  const [transcription, setTranscription] = useState('');
-  const [sentiment, setSentiment] = useState('');
+  const [files, setFiles] = useState([]);
+  const [results, setResults] = useState([]);
   const [error, setError] = useState('');
-  const [score, setScore] = useState('')
+  const [csvLink, setCsvLink] = useState('');
+  const [previewLink, setPreviewLink] = useState('');
+  const [text, setText] = useState('Upload Files For Transcription')
+  const [processed, setProcessed] = useState(false)
   const handleFileChange = (event) => {
-    setFile(event.target.files[0]);
-    setFileName(event.target.files[0].name);
+    setFiles(event.target.files);
   };
 
   const handleUpload = async () => {
     const formData = new FormData();
-    formData.append('file', file);
+    for (let i = 0; i < files.length; i++) {
+      formData.append('files', files[i]);
+    }
+    setText('Uploading...')
 
     try {
       const response = await axios.post('http://localhost:5000/transcribe_and_analyze', formData, {
@@ -23,65 +27,72 @@ const App = () => {
           'Content-Type': 'multipart/form-data'
         }
       });
-      console.log(response)
-      const { file_name, text_infile, sentiment_infile, error } = response.data;
+
+      console.log(response.data);
+      setProcessed(true)
+      const { results, error } = response.data;
       if (error) {
         setError(error);
       } else {
-        setFileName(file_name);
-        setTranscription(text_infile);
-        setSentiment(sentiment_infile[0]?.label);
-        setScore(sentiment_infile[0]?.score)
+        setResults(results);
+        setCsvLink(response.data.csv_link);
       }
     } catch (error) {
-      setError('Error uploading file. Please try again.');
+      setError('Error uploading files. Please try again.');
       console.error(error);
     }
   };
-
+  const handleExtractCSV = () => {
+    window.location.href = csvLink;
+  }
   return (
     <div className="App">
-      <section class="sticky">
-  <div class="bubbles">
-      <div class="bubble"></div>
-    <div class="bubble"></div>
-    <div class="bubble"></div>
-    <div class="bubble"></div>
-    <div class="bubble"></div>
-    <div class="bubble"></div>
-    <div class="bubble"></div>
-    <div class="bubble"></div>
-    <div class="bubble"></div>
-    <div class="bubble"></div>
-    
-  </div>
-</section>
+      <section className="sticky">
+        <div className="bubbles">
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+          <div className="bubble"></div>
+        </div>
+      </section>
       <div className="heading">
-      <h1>Speech Transcription & Sentiment Analysis</h1>
+        <h1>Speech Transcription & Sentiment Analysis</h1>
       </div>
       <div className="btnWrapper">
-      <input type="file" onChange={handleFileChange} accept=".wav" id='kuchBhi'/>
-      <button onClick={handleUpload} className='btn'>Upload File</button>
-
-      {error && <p style={{ color: 'red', position : 'absolute' }}>{error}</p>}
+        <input type="file" onChange={handleFileChange} multiple id='kuchBhi' />
+        <button onClick={handleUpload} className='btn'>Upload Files</button>
+        {error && <p style={{ color: 'red', position: 'absolute' }}>{error}</p>}
       </div>
-      <div className="responseWrapper">
-        {!fileName && <p className='placeholder'>Upload A File For Transcription</p>}
-      {fileName && (
-        <div>
-          <h2>File Name: {fileName}</h2>
-          <div className="transcription">
-          <span className='textLabel'>Transcription:</span>
-          <span className='textResponse'>{transcription}</span></div>
-          <div className="sentiment">
-          <span className='sentimentLabel'>Sentiment Analysis:</span>
-          <span className='sentimentResponse'>{sentiment}</span></div>
-          <div className="score">
-          <span className='scoreLabel'>Score:</span>
-          <span className='scoreResponse'>{score}</span></div>
+      <div className="responseWrapper1">
+        {results.length === 0 && <p className='placeholder'>{text}</p>}</div>
+        <div className="responses">
+        {results.length > 0 && results.map((result, index) => (
+      <div className="responseWrapper" key={index}>
+          {/* <div> */}
+            <h2>File Name: {result.file_name}</h2>
+            <div className="transcription">
+              <span className='textLabel'>Transcription:</span>
+              <span className='textResponse'>{result.transcription}</span>
+            </div>
+            <div className="sentiment">
+              <span className='sentimentLabel'>Sentiment Analysis:</span>
+              <span className='sentimentResponse'>{result.sentiment.label}</span>
+            </div>
+            <div className="score">
+              <span className='scoreLabel'>Score:</span>
+              <span className='scoreResponse'>{result.sentiment.score}</span>
+            </div>
+          </div>
+      // </div>
+        ))}
         </div>
-      )}
-      </div>
+        <button className="downloadBtn btn" onClick={handleExtractCSV} disabled={!processed}>Extract CSV</button>
     </div>
   );
 };
